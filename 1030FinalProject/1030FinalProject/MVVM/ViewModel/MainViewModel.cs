f5d6d99c44165aa7d0941ac760a077e14ba1130f
Lane McAuley <hallowz@hotmail.com>
2021-12-15 19:42:59 -0500

    Added basic GUI functions

---
+++ 1030FinalProject/1030FinalProject/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _1030FinalProject.Core;
+
+namespace _1030FinalProject.MVVM.ViewModel
+{
+
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+        }
+    }
+}
